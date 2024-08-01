73c57ccc1d70e424429729c5c008ac35c738e65c
Sandra <sandra.youssoufh@gmail.com>
2024-08-01 12:12:47 +0200

    Formation Page Configuration

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,14 +5,27 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Formation;
+use Doctrine\ORM\EntityManagerInterface;
 
 class HomeController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager) 
+    {
+        $this->entityManager = $entityManager;
+    }
+
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+        $repository = $this->entityManager->getRepository(Formation::class);
+        $formations = $repository->findAll();
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'formation' => $formations,
         ]);
     }
 }
